902a58346b29f74a6ef64856000e5cdc31907b6b
jbuskiewicz <jakub.buskiewicz@silkycoders.com>
2021-12-05 15:53:08 +0100

    events

--- html5/scripts/main.js
+++ html5/scripts/main.js
@@ -28,11 +28,16 @@ const headerContainer = document.querySelector(".topHeader");
 
 // headerContainer.innerHTML = "<p>test</p>";
 
+// "pobieramy" kontener "main" (<main>) do zmiennej
+const mainContainer = document.querySelector("main");
+
 document.onscroll = function () {
     if (document.documentElement.scrollTop > 100) {
-        // headerContainer.classList.add("");
+        headerContainer.classList.add("headerFixed");
+        mainContainer.classList.add("mainMarginTop");
     } else {
-        // headerContainer.classList.remove("");
+        headerContainer.classList.remove("headerFixed");
+        mainContainer.classList.remove("mainMarginTop");
     }
 }
 
@@ -42,3 +47,22 @@ document.onscroll = function () {
 // CSS
 // musimy stworzyć dwie klasy - jedną dla headera (która nada position fixed etc)
 // drugą dla maina, która doda margin-top: 100px
+
+
+// EVENTY
+const form = document.querySelector("#contactForm");
+
+// nasłuchujemy na event "submit" na formularzu
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); // wyłączenie domyślnego zachowania eventu - w tym przypadku wysyłki
+
+    let isFormValid = false;
+
+    // walidacja
+    // ....
+
+    // event.target[0].value - wartość pierwszego inputa w naszym formularzu
+    console.log(event.target[0].value);
+
+    if (isFormValid) event.submit();
+});
